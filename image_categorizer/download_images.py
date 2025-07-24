06c465462395433b4243d44e8ab0fa8898cf2211
James <jamesas1210@gmail.com>
2025-07-24 14:59:14 +0100

    Download images

--- image_categorizer/download_images.py
+++ image_categorizer/download_images.py
@@ -2,7 +2,7 @@ from simple_image_download import simple_image_download as sim
 
 response = sim.simple_image_download
 
-search_terms = ["mens casual outfits", "mens formal outfits"]
+search_terms = ["young mens fashion", "mens winter outfits", "mens casual outfits", "mens formal outfits"]
 # Terms to search for images
 
 for term in search_terms:
